763d625ce60876ea65a683249638a373fe20973f
AlexTheGeek <28763288+AlexTheGeek@users.noreply.github.com>
2023-12-06 16:18:07 +0100

    passage en port 5001

--- main.py
+++ main.py
@@ -350,4 +350,4 @@ def get_template_info(uuid):
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-    app.run(debug=True, host="0.0.0.0")
+    app.run(debug=True, host="0.0.0.0", port=5001)

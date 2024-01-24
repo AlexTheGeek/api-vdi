39af7ff7e420d7beb60b628b9985f91386c2047f
AlexTheGeek <28763288+AlexTheGeek@users.noreply.github.com>
2024-01-24 12:16:14 +0100

    defaults account

--- main.py
+++ main.py
@@ -713,16 +713,45 @@ if __name__ == '__main__':
         logger.info('Creating DB')
         db.create_all()
         db.session.commit()
-        # Create default admin user
+        # Create openstack user
         if not User.query.filter_by(email="openstack@insa-cvl.fr").first():
-            logger.info('Creating default admin user')
+            logger.info('Creating openstack user')
             random_password = get_random_string(15)
             hashed_password = PasswordHasher().hash(random_password) 
             new_user = User(id="1", email="openstack@insa-cvl.fr", first_name="openstack", last_name="openstack",
                             password=hashed_password, role="admin", cas=True)
             db.session.add(new_user)
             db.session.commit()
+            logger.info("Openstack user created with password: "+random_password)
+        # Create default admin user
+        if not User.query.filter_by(email="admin@admin.fr").first():
+            logger.info('Creating default admin user')
+            random_password = "admin"
+            hashed_password = PasswordHasher().hash(random_password) 
+            new_user = User(id="2", email="admin@admin.fr", first_name="Admin", last_name="VDI",
+                            password=hashed_password, role="admin", cas=False)
+            db.session.add(new_user)
+            db.session.commit()
             logger.info("Default admin user created with password: "+random_password)
-            # print("Default admin user created with password: "+random_password)
+        # Create default prof user
+        if not User.query.filter_by(email="prof@prof.fr").first():
+            logger.info('Creating default prof user')
+            random_password = "prof"
+            hashed_password = PasswordHasher().hash(random_password) 
+            new_user = User(id="3", email="prof@prof.fr", first_name="Prof", last_name="VDI",
+                            password=hashed_password, role="prof", cas=False)
+            db.session.add(new_user)
+            db.session.commit()
+            logger.info("Default prof user created with password: "+random_password)
+        # Create default etudiant user
+        if not User.query.filter_by(email="etudiant@etudiant.fr").first():
+            logger.info('Creating default etudiant user')
+            random_password = "etudiant"
+            hashed_password = PasswordHasher().hash(random_password) 
+            new_user = User(id="4", email="etudiant@etudiant.fr", first_name="Etudiant", last_name="VDI",
+                            password=hashed_password, role="user", cas=False)
+            db.session.add(new_user)
+            db.session.commit()
+            logger.info("Default etudiant user created with password: "+random_password)
     logger.info('API started')
     app.run(debug=True, host="0.0.0.0", port=5001)
--- wsgi.py
+++ wsgi.py
@@ -36,7 +36,7 @@ if __name__ == "__main__":
         logger.info('Creating DB')
         db.create_all()
         db.session.commit()
-        # Create default admin user
+        # Create openstack user
         if not User.query.filter_by(email="openstack@insa-cvl.fr").first():
             random_password = get_random_string(15)
             hashed_password = PasswordHasher().hash(random_password) 
@@ -45,6 +45,35 @@ if __name__ == "__main__":
             db.session.add(new_user)
             db.session.commit()
             logger.info("Default admin user created with password: "+random_password)
-            # print("Default admin user created with password: "+random_password)
+        # Create default admin user
+        if not User.query.filter_by(email="admin@admin.fr").first():
+            logger.info('Creating default admin user')
+            random_password = "admin"
+            hashed_password = PasswordHasher().hash(random_password) 
+            new_user = User(id="2", email="admin@admin.fr", first_name="Admin", last_name="VDI",
+                            password=hashed_password, role="admin", cas=False)
+            db.session.add(new_user)
+            db.session.commit()
+            logger.info("Default admin user created with password: "+random_password)
+        # Create default prof user
+        if not User.query.filter_by(email="prof@prof.fr").first():
+            logger.info('Creating default prof user')
+            random_password = "prof"
+            hashed_password = PasswordHasher().hash(random_password) 
+            new_user = User(id="3", email="prof@prof.fr", first_name="Prof", last_name="VDI",
+                            password=hashed_password, role="prof", cas=False)
+            db.session.add(new_user)
+            db.session.commit()
+            logger.info("Default prof user created with password: "+random_password)
+        # Create default etudiant user
+        if not User.query.filter_by(email="etudiant@etudiant.fr").first():
+            logger.info('Creating default etudiant user')
+            random_password = "etudiant"
+            hashed_password = PasswordHasher().hash(random_password) 
+            new_user = User(id="4", email="etudiant@etudiant.fr", first_name="Etudiant", last_name="VDI",
+                            password=hashed_password, role="user", cas=False)
+            db.session.add(new_user)
+            db.session.commit()
+            logger.info("Default etudiant user created with password: "+random_password)
     logger.info('API started')
     app.run()

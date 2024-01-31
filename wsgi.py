898e9b12e92d50f8a3f310d849f7335ee59edbe8
AlexTheGeek <28763288+AlexTheGeek@users.noreply.github.com>
2024-01-31 15:47:31 +0100

    db migrate ?

--- wsgi.py
+++ wsgi.py
@@ -2,9 +2,15 @@
 ### Librairies ###
 ##################
 from main import app, db, User, get_random_string
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 from argon2 import PasswordHasher
 import logging
 import os
+
+
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 ##################
 ### Log config ###

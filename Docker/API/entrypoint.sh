25ff8ccc85828cf1b740b255f429fb9afde40385
AlexTheGeek <28763288+AlexTheGeek@users.noreply.github.com>
2024-01-31 19:26:40 +0100

    Docker wip

---
+++ Docker/API/entrypoint.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+if [[ ! -d "/var/log/VDI/API" ]]; then
+    mkdir -p /var/log/VDI/API
+    touch /var/log/VDI/API/access.log
+    touch /var/log/VDI/API/error.log
+    touch /var/log/VDI/API/api-flask.log
+fi
+
+gunicorn --access-logfile /var/log/VDI/API/access.log --error-logfile /var/log/VDI/API/error.log --workers 3 --bind 0.0.0.0:5001 main:app

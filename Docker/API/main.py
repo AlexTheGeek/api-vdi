ff927f7fc7297576907bfb34195c0dc00e0c6593
AlexTheGeek <28763288+AlexTheGeek@users.noreply.github.com>
2024-02-01 15:10:34 +0100

    test

--- Docker/API/main.py
+++ Docker/API/main.py
@@ -27,6 +27,7 @@ DB_USER = os.getenv('DB_USER')
 DB_PASSWORD = os.getenv('DB_PASSWORD')
 DB_HOST = os.getenv('DB_HOST')
 DB_NAME = os.getenv('DB_NAME')
+print(DB_NAME)
 
 ################
 ### Vars APP ###
